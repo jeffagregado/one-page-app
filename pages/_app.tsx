5f2fdd9c6687b95573ff3d8fb82c4e7fe0dc51f8
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-02-20 16:33:28 +0800

    added important rule with tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-import '../styles/globals.scss'
+import '../styles/tailwind.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
