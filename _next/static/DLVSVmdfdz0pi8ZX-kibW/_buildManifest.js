17c5a358522ad43edc69c39cef7caaa172f701b8
jeffagregado <jeffagregado@gmail.com>
2021-04-14 16:35:02 +0000

    Deploying to gh-pages from @ jeffagregado/one-page-app@df62edd8c7678a0ef09cc740a474f98463c7895d 🚀

--- _next/static/DLVSVmdfdz0pi8ZX-kibW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fa5a855d3dad7b9b89e6d.css","static\u002Fchunks\u002Fpages\u002Findex-6ca90af2d5bc66519225.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e8b5a60cc98a312527aa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
